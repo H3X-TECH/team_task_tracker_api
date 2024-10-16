06de6e6c089cf0205525f2eea1fde71b44b9fb25
Kyaw Zin Lin <kyawzin.lin@kbzbank.com>
2024-10-16 15:10:50 +0630

    share init and task init

--- TaskService.Domain/Entities/Information/Task.cs
+++ TaskService.Domain/Entities/Information/Task.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskService.Domain.Enums;
 
 namespace TaskService.Domain.Entities.Information
 {
     public class Task : BaseEntity
     {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; } 
+        public Status Status { get; set; }
+        public string ProjectId { get; set; }
+        public string AssignedTo { get; set; }
+        public DateTime DueDate { get; set; }
     }
 }
---
+++ TaskService.Domain/Enums/Status.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskService.Domain.Enums
+{
+    public enum Status
+    {
+        ToDo = 1,
+        InProgress,
+        Blocked,
+        Complete,
+        NeedsRevision,
+        Approved,
+        FinalComplete,
+        Cancelled
+    }
+}
