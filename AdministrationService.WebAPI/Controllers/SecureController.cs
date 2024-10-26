52eb795e9a285a30134e421ebeed82b2484b834b
Kyaw Zin Lin <kyawzin.lin@kbzbank.com>
2024-10-26 23:16:30 +0630

    feat; secure end point

---
+++ AdministrationService.WebAPI/Controllers/SecureController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdministrationService.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class SecureController : ControllerBase
+    {
+        [HttpGet("protected")]
+        public IActionResult GetProtectedData()
+        {
+            return Ok("This is protected data!");
+        }
+    }
+}
---
+++ AdministrationService.WebAPI/Controllers/UserAccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdministrationService.WebAPI.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
